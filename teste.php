26b8843233719337c8978aae44d1b7e586db94cc
Natália Nascimento <natalia.nascc@gmail.com>
2021-03-03 17:25:38 -0300

    Exercício php

---
+++ teste.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name = "viewport" content ="width=device-width, initial-scale=1">
+<title>Calculo de Consumo de Combustível</title>
+</head>
+
+<h1>Olá mundão!</h1>
+</html>
