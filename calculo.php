9fbe5d356c1334865ce107ca8224045120abbdc9
Natália Nascimento <natalia.nascc@gmail.com>
2021-03-04 14:51:21 -0300

    Atualização Final

--- calculo.php
+++ calculo.php
@@ -1,3 +1,62 @@
 <?php 
 
-echo $_POST ['distancia'];
+$mensagem ="";
+
+if ($_POST) {
+    $distancia = $_POST['distancia'];
+    $autonomia = $_POST['autonomia'];
+    
+    if (is_numeric($distancia) && is_numeric ($autonomia)) {
+        if ($distancia > 0 && $autonomia > 0){
+            $valorGasolina = 4.80;
+            $valorAlcool = 3.80;
+            $valorDiesel = 3.00;
+            
+            $calculoGasolina = ($distancia / $autonomia) * $valorGasolina;
+            $calculoGasolina = number_format ($calculoGasolina, 2, ',','.');
+            
+            $calculoAlcool = ($distancia / $autonomia) * $valorAlcool;
+            $calculoAlcool = number_format ($calculoAlcool, 2, ',','.');
+            
+            $calculoDiesel = ($distancia / $autonomia) * $valorDiesel;
+            $calculoDiesel = number_format ($calculoDiesel, 2, ',','.');
+            
+            $mensagem.= "<p>O valor do consumo em Gasolina foi de: R$".$calculoGasolina."</p>";
+            $mensagem.= "<p>O valor do consumo em Alcool foi de: R$".$calculoAlcool."</p>";
+            $mensagem.= "<p>O valor do consumo em Diesel foi de: R$".$calculoDiesel."</p>";
+        } else {
+            $mensagem = "<p>O valor recebido não é um número válido.</p>";
+        }
+       
+    } else {
+        $mensagem = "<p>O valor da distância e da autonomia deve ser maior que zero.</p>";
+    }
+    
+} else {
+    echo "<p>Nenhum dado foi recebido pelo formulário.</p>";
+}
+?> 
+
+<!DOCTYPE HTML>
+<HTML lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, inicial-scale=1">
+<title>Cálculo de Consumo de Combustível</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<main>
+		<div class="painel">
+			<h2>Cálculo de Consumo de Combustível</h2>
+			<div class="conteudo-painel">
+				<?php 
+				echo $mensagem;
+				?>
+				<a class="botao" href="index.php">Voltar</a>
+			</div>
+		</div>	
+	</main>
+</body>
+</HTML>
+
--- index.php
+++ index.php
@@ -4,14 +4,14 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name = "viewport" content ="width=device-width, initial-scale=1">
     <title>Calculo de Consumo de Combustível</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
   </head>
   <body>
     <main>
     	<div class = "painel">
     		<h2>Instruções</h2>
     		<div class= "conteudo-painel">
-    			<p>Esta aplicação tem como finalidade demonstrar os valores que serão gastos com combustível durante uma viagem, com base no consumo do seu veículo, e com a distânica determinada por você!</p>
+    			<p>Esta aplicação tem como finalidade demonstrar os valores que serão gastos com combustível durante uma viagem, 				com base no consumo do seu veículo, e com a distânica determinada por você!</p>
     			<p>Os combustíveis disponíveis para este cálculo são:</p>
     			<ul>
     				<li><b>Álcool</b></li>
@@ -21,27 +21,20 @@
     		</div>
     	</div>
     
-    <div class="painel">
-    	<h2>Cálculo do valor (R$) do consumo</h2>
-    	<div class="conteudo-painel">
-    	<form action="calculo.php" method="POST">
-    	<label for="distancia">Distância em kilômetros a ser percorrida</label>
-    	<input type="text" name="distancia"/>
-    	
-    	<label for="autonomia">Consumo de combustível do veículo (Km/l)</label>
-    	<input type="text" name="autonomia"/>
-    	<button class="botao" type="submit"> Calcular</button>
-    	</form>
-    	
-    	</div>
-    </div>
-    
+        <div class="painel">
+        	<h2>Cálculo do valor (R$) do consumo</h2>
+        	
+        	<div class="conteudo-painel">
+        	    <form action="calculo.php" method="POST">
+        			<label for="distancia">Distância em kilômetros a ser percorrida</label>
+        			<input type="number" class="campoTexto" name="distancia" required/>
+        	        <label for="autonomia">Consumo de combustível do veículo (Km/l)</label>
+        			<input type="number" class="campoTexto" name="autonomia" required/>
+        	        <button class="botao" type="submit"> Calcular</button>
+        		</form>
+        	</div>
+        	
+        </div>
     </main>
-<?php
-$mensagem = "<h1>Ola Mundo!</h1>";
-echo $mensagem;
-echo "<h2>hello</h2>";
-?>
-
 </body>
 </html>
